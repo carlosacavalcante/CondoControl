2fd5d628100c6b4499693bb3bb8250b9cbd8ce97
carlos <carlosacfilho1992@gmail.com>
2026-03-06 22:26:38 -0300

    commit de criacao do projeto

---
+++ src/main/java/io/github/carlosacavalcante/CondoControl/CondoControlApplication.java
@@ -0,0 +1,13 @@
+package io.github.carlosacavalcante.CondoControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CondoControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CondoControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/carlosacavalcante/CondoControl/CondoControlApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.carlosacavalcante.CondoControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CondoControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
